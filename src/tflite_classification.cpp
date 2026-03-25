887276c70735d47f46c8df0ecb7a3d88e905a2bf
Burak123T <burak-9@live.dk>
2026-03-25 15:07:55 +0100

    removed 'free'

--- src/tflite_classification.cpp
+++ src/tflite_classification.cpp
@@ -148,7 +148,7 @@ void run_classif()
         }
 
         // uint8_t *scores = interpreter->typed_output_tensor<uint8_t>(0);
-        float max_val = -1.0;
+        float max_val = -1.0;ß
         int max_cls = -1;
         uint8_t *scores = interpreter->typed_output_tensor<uint8_t>(0);
         for (int j = 0; j < output_size; ++j)
@@ -181,7 +181,7 @@ void run_classif()
         }
 
         // Free the input image
-        free_test(input_image_data);
+        //free_test(input_image_data);
     }
 }
 
